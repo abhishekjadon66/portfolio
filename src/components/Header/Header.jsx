c1eec647092f70e15135d33afbc28c070e8aa57f
abhishekjadon66 <jadonabhishek332@gmail.com>
2023-08-04 01:49:29 +0530

    this

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from 'react'
-
+import React, { useRef, useEffect, useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 const Header = () => {
+    const [open, setOpen] = useState(false);
     const headerRef = useRef(null)
     const menuRef = useRef(null)
 
@@ -42,7 +43,7 @@ const Header = () => {
                             AJ
                         </span>
                         <div className="leading-[20px]">
-                            <h2 className='text-xl text-smallTextColor font-[700]'>Abhishek Jadon</h2>
+                            <h2 className='text-lg text-smallTextColor font-[700]'>Abhishek Jadon</h2>
                             <p className='text-smallTextColor text-[14px] font-[500]'>Portfolio</p>
                         </div>
                     </div>
@@ -50,29 +51,36 @@ const Header = () => {
 
                     {/* =============menu start============= */}
 
-                    <div className="menu hidden" onClick={toggleMenu} ref={menuRef}>
-                        <ul className='flex items-center gap-10  '>
+                    <div onClick={() => setOpen(!open)} className="menu lg:block md:pr-6">
+                        <ul className={`md:flex md:items-center gap-9 md:pb-0 pb-12 absolute md:static bg-white left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in cursor-pointer z-10 ${open ? "top-[4rem]" : "top-[-490px]"
+                            }`}>
                             <li><a onClick={handleClick} className='text-smallTextColor font-[400]' href="#about">About</a></li>
                             <li><a onClick={handleClick} className='text-smallTextColor font-[400]' href="#services">Services</a></li>
                             <li><a onClick={handleClick} className='text-smallTextColor font-[400]' href="#portfolio">Portfolio</a></li>
                             <li><a onClick={handleClick} className='text-smallTextColor font-[400]' href="#contact">Contact</a></li>
                         </ul>
-                    </div>
+                    </div >
                     {/* =============menu end============= */}
 
 
                     {/* =============menu right============= */}
-                    <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-4 mr-6 md:mr-0">
                         <button className='flex items-center gap-2 text-smallTextColor font-[400] border border-solid border-smallTextColor py-2 md:px-4 px-2 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[300]'>
                             <i class="ri-send-plane-line"></i>Let's Talk
                         </button> </div>
 
-                    <span onClick={toggleMenu} className='text-2xl text-smallTextColor md:hidden cursor-pointer'><i class="ri-menu-line"></i></span>
+                    <div
+                        onClick={() => setOpen(!open)}
+                        className="text-2xl absolute right-3 top-7 cursor-pointer md:hidden"
+                    >
+                        {open ? <AiOutlineClose /> : <AiOutlineMenu />}
+                    </div>
+
                     {/* =============menu rigt end============= */}
 
-                </div>
-            </div>
-        </header>
+                </div >
+            </div >
+        </header >
     )
 }
 
